2ed012bd7b64eddfb43b0afadc89252a5c0c8c50
Ben-1819 <ben.brown20332@gmail.com>
2025-05-23 14:38:16 +0100

    feat: Added the registration tests
    Added the registration tests and made sure they all passed

--- FriendPointsBackend/tests/Feature/RegisterTest.php
+++ FriendPointsBackend/tests/Feature/RegisterTest.php
@@ -13,7 +13,7 @@ describe("Tests to check that the register method works as intented", function()
 
     // After each test
     afterEach(function(){
-
+        log::info("test in RegisterTests group completed");
     });
 
     /**
@@ -21,15 +21,39 @@ describe("Tests to check that the register method works as intented", function()
      * valid input data
      */
     it("tests that users can register for an account when using valid input data", function(){
+        // Send a post request to the register route
+        $response = $this->postJson("/api/register",[
+            "first_name" => "test",
+            "last_name" => "user",
+            "email" => "test@gmail.com",
+            "password" => "password",
+        ]);
 
+        // Declare what the response should be
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                "token",
+                "user",
+            ]);
     });
-
     /**
      * Test that users cannot register for an account when
      * invalid data is entered in the first_name field
      */
     it("tests that users cannot register for an account when invalid data is entered in the first_name field", function(){
+        // Send a post request to the register route
+        $response = $this->postJson("/api/register",[
+            "first_name" => "",
+            "last_name" => "User",
+            "email" => "email@gmail.com",
+            "password" => "password",
+        ]);
 
+        // Declare what the response should be
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors([
+                "first_name" => "First name is a required field",
+            ]);
     });
 
     /**
@@ -37,7 +61,19 @@ describe("Tests to check that the register method works as intented", function()
      * invalid data is entered in the last_name field
      */
     it("tests that users cannot register for an account when invalid data is entered in the last_name field", function(){
+        // Send a post request to the register route
+        $response = $this->postJson("/api/register",[
+            "first_name" => "Test",
+            "last_name" => "",
+            "email" => "email@gmail.com",
+            "password" => "password",
+        ]);
 
+        // Declare what the response should be
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors([
+                "last_name" => "Last name is a required field",
+            ]);
     });
 
     /**
@@ -45,7 +81,19 @@ describe("Tests to check that the register method works as intented", function()
      * invalid data is entered in the email field
      */
     it("tests that users can not register when invalid data is entered in the email field", function(){
+        // Make a request to the register route
+        $response = $this->postJson("/api/register", [
+            "first_name" => "Test",
+            "last_name" => "User",
+            "email" => "",
+            "password" => "password",
+        ]);
 
+        // Declare what the repsonse should be
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors([
+                "email" => "Email is a required field",
+            ]);
     });
 
     /**
@@ -53,6 +101,18 @@ describe("Tests to check that the register method works as intented", function()
      * invalid data is entered in the password field
      */
     it("tests that users can not register for an account when invalid data is entered in the password field", function(){
+        // Make a request to the register route
+        $response = $this->postJson("/api/register",[
+            "first_name" => "Test",
+            "last_name" => "User",
+            "email" => "example@gmail.com",
+            "password" => "",
+        ]);
 
+        // Declare what the response should be
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors([
+                "password" => "Password is a required field",
+            ]);
     });
 })->group("RegisterTests");
