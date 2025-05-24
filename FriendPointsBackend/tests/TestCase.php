747823fd53cca89bde7e99e8c80a9d4297da30f0
Ben-1819 <ben.brown20332@gmail.com>
2025-05-24 14:07:07 +0100

    feat: Manually registered JWTMiddleware alias for testing
    Added jwt middleware alias registration in TestCase.php to ensure JWT
    protected routes work as expected in test environment.

--- FriendPointsBackend/tests/TestCase.php
+++ FriendPointsBackend/tests/TestCase.php
@@ -2,9 +2,16 @@
 
 namespace Tests;
 
+use App\Http\Middleware\JWTMiddleware;
+
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        app('router')->aliasMiddleware('jwt', JWTMiddleware::class);
+    }
 }
