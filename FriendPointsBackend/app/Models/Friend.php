a4ff01ff27e833e34e9ce25b1eed689987be045d
Ben-1819 <ben.brown20332@gmail.com>
2025-06-02 16:38:35 +0100

    feat: Created the FriendFactory

--- FriendPointsBackend/app/Models/Friend.php
+++ FriendPointsBackend/app/Models/Friend.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Friend extends Model
 {
+
+    /** @use HasFactory<\Database\Factories\FriendFactory> */
+    use HasFactory;
+
     protected $fillable = [
         "id",
         "user_id",
