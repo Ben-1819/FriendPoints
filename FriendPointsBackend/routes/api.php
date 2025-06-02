5a54bf9666d53e49ee7e63170dad40c0b4676c1d
Ben-1819 <ben.brown20332@gmail.com>
2025-06-02 16:38:59 +0100

    feat: Added friend routes to api.php
    Added the friend routes to api.php, these routes are linked to methods
    inside the FriendController.

--- FriendPointsBackend/routes/api.php
+++ FriendPointsBackend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FriendController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -11,4 +12,22 @@ Route::controller(AuthController::class)->group(function(){
 Route::controller(AuthController::class)->middleware("jwt")->group(function(){
     Route::get("/user", "getUser");
     Route::post("/logout", "logout");
+});
+
+Route::controller(FriendController::class)->middleware("jwt")->group(function(){
+    Route::get("/index", "index");
+    Route::get("/group1/index", "group1Friends");
+    Route::get("/group2/index", "group2Friends");
+    Route::get("/{id}/index", "otherIndex");
+    Route::get("/{id}/group1/index", "otherGroup1Friends");
+    Route::get("/{id}/group2/index", "otherGroup2Friends");
+    Route::get("/showOptions", "showOptions");
+    Route::post("/store", "store");
+});
+
+Route::controller(FriendController::class)->middleware(["jwt", "friendOwner"])->group(function(){
+    Route::put("/{id}/update", "update");
+    Route::put("/{id}/addPoints", "addPoints");
+    Route::put("/{id}/removePoints", "removePoints");
+    Route::put("/{id}/delete", "destroy");
 });
