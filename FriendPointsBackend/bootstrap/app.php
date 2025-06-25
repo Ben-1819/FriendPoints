2a7ff45b232c94a2a5cd2d82583c6abf112b944e
Ben-1819 <ben.brown20332@gmail.com>
2025-06-25 16:03:58 +0100

    feat: Gave the HistoryOwner middleware an alias

--- FriendPointsBackend/bootstrap/app.php
+++ FriendPointsBackend/bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\JWTMiddleware;
 use App\Http\Middleware\FriendOwner;
+use App\Http\Middleware\HistoryOwner;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             "jwt" => JwtMiddleware::class,
             "friendOwner" => FriendOwner::class,
+            "historyOwner" => HistoryOwner::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
