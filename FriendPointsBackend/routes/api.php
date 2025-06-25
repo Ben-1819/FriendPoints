3de7dbd72b8015c1d07ec0fb96c07ad76ac3d151
Ben-1819 <ben.brown20332@gmail.com>
2025-06-25 16:08:23 +0100

    feat: Added the History routes
    Added the History routes to api.php. Group them correctly and applied
    the correct middleware to each group.

--- FriendPointsBackend/routes/api.php
+++ FriendPointsBackend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\FriendController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HistoryController;
 
 Route::controller(AuthController::class)->group(function(){
     Route::post("/register", "register");
@@ -30,4 +31,16 @@ Route::controller(FriendController::class)->middleware(["jwt", "friendOwner"])->
     Route::put("/{id}/addPoints", "addPoints");
     Route::put("/{id}/removePoints", "removePoints");
     Route::delete("/{id}/delete", "destroy");
+});
+
+Route::controller(HistoryController::class)->middleware("jwt")->group(function(){
+    Route::get("/history/index", "index");
+    Route::get("/history/{id}/FriendIndex", "friendIndex");
+    Route::get("/history/{id}/show", "show");
+});
+
+Route::controller(HistoryController::class)->middleware(["jwt", "historyOwner"])->group(function(){
+    Route::post("/history/store", "store");
+    Route::put("/history/{id}/update", "update");
+    Route::delete("/history/{id}/delete", "destroy");
 });
