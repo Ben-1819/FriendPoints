083decab837286c67f6f730684e76ff7fc94c96b
Ben-1819 <ben.brown20332@gmail.com>
2025-07-07 10:42:39 +0100

    feat: Created the auth store
    Created the auth store, this store will be used to keep track of the
    user that is currently logged into the application.

---
+++ FriendPoints-Frontend/src/stores/auth.js
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    user: null,
+    token: null,
+  }),
+  actions: () => ({
+    setUser(user) {
+      this.user = user;
+    },
+    setToken(token) {
+      this.token = token;
+    },
+    logout() {
+      this.user = null;
+      this.token = null;
+      localStorage.removeItem("token");
+    },
+    async fetchUser() {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/user", {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        });
+        this.user = response.data;
+      } catch (error) {
+        this.logout();
+      }
+    },
+  }),
+});
