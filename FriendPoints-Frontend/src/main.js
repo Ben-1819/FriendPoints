d34d48c757b2da5908039b93fd12d86a7fc9702e
Ben-1819 <ben.brown20332@gmail.com>
2025-07-07 10:20:52 +0100

    feat: Setup main.js file
    Setup the main.js file to use pinia, vue-router and axios.

--- FriendPoints-Frontend/src/main.js
+++ FriendPoints-Frontend/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import axios from "axios";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+window.axios = axios;
+createApp(App).use(createPinia()).mount("#app");
