ec063c7517d646253e342b53187c03895007ea04
Ben-1819 <ben.brown20332@gmail.com>
2025-07-08 16:48:26 +0100

    feat: Added setup for using Vue-Router to main.js.

--- FriendPoints-Frontend/src/main.js
+++ FriendPoints-Frontend/src/main.js
@@ -2,8 +2,14 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createRouter, createWebHistory } from "vue-router";
 import axios from "axios";
 import App from "./App.vue";
 
 window.axios = axios;
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
 createApp(App).use(createPinia()).mount("#app");
