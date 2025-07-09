1eb1717ba36528714c0b0c7e4076173d30e1f566
Ben-1819 <ben.brown20332@gmail.com>
2025-07-09 09:53:19 +0100

    feat: Wrote a test for the register component.

--- FriendPoints-Frontend/cypress/component/RegisterComponent.cy.js
+++ FriendPoints-Frontend/cypress/component/RegisterComponent.cy.js
@@ -1,5 +1,27 @@
-describe('RegisterComponent.cy.js', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
-})
+import Register from "@/components/Register.vue";
+
+describe("Tests to ensure the first_name error messages are returned when invalid data is entered into the first_name field", () => {
+  it("tests the correct error message is returned when the first name field is left blank in the register component", () => {
+    cy.mount(Register);
+
+    // Check the error messages do not exist currently
+    cy.noErrors();
+
+    // Call the custom register command
+    cy.register({
+      // Pass null in as the first_name parameter
+      first_name: null,
+      last_name: "Brown",
+      email: "ben@gmail.com",
+      password: "password",
+    });
+
+    // Check the first name error message was returned
+    cy.shouldShowFieldError("firstName", "First name is a required field");
+
+    // Check there are no other error messages
+    cy.noLastNameErrors();
+    cy.noEmailErrors();
+    cy.noPasswordErrors();
+  });
+});
