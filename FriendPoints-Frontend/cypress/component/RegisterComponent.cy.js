fa3be90fc2336090e034aed8237882ae22835a2f
Ben-1819 <ben.brown20332@gmail.com>
2025-07-14 20:03:08 +0100

    feat: Completed writing tests for last name errors
    Completed writing tests that will check that the correct error messages
    are returned when the validation rules for the last name field are
    broken.

--- FriendPoints-Frontend/cypress/component/RegisterComponent.cy.js
+++ FriendPoints-Frontend/cypress/component/RegisterComponent.cy.js
@@ -76,10 +76,108 @@ describe("Tests to ensure the first_name error messages are returned when invali
     // Get the password field and enter valid data into it
     cy.get('[data-cy="passwordInput"]').type("password");
 
+    // Click the submit button
+    cy.get('[data-cy="register-button"]').click();
+
     // Check the first name error message was returned
     cy.shouldShowFieldError(
       "firstName",
       "First name can not be longer than 55 characters"
     );
+
+    // Check there are no other error messages
+    cy.noLastNameErrors();
+    cy.noEmailErrors();
+    cy.noPasswordErrors();
+  });
+});
+
+describe("Tests to ensure the last_name error messages are returned when invalid data is entered into the last_name field", () => {
+  it("tests the correct error message is returned when the last_name field is left empty", () => {
+    // Mount the register component
+    cy.mount(Register);
+
+    // Check there are no error messages
+    cy.noErrors();
+
+    // Call the custom register command
+    cy.register({
+      first_name: "Ben",
+      // Leave the last_name field empty
+      last_name: "",
+      email: "ben@gmail.com",
+      password: "password",
+    });
+
+    // Check the last name error message was returned
+    cy.shouldShowFieldError("lastName", "Last name is a required field");
+
+    // Check there are no other error messages
+    cy.noFirstNameErrors();
+    cy.noEmailErrors();
+    cy.noPasswordErrors();
+  });
+
+  it("tests the correct error message is returned when the data entered into the last_name field is not a string value", () => {
+    // Mount the register component
+    cy.mount(Register);
+
+    // Check there are no error messages
+    cy.noErrors();
+
+    // Call the custom register command
+    cy.register({
+      first_name: "Ben",
+      // Enter an integer into the last_name field
+      last_name: 1,
+      email: "ben@gmail.com",
+      password: "password",
+    });
+
+    // Check the last name error message was returned
+    cy.shouldShowFieldError(
+      "lastName",
+      "Last name must be of data type string"
+    );
+
+    // Check there are no other error messages
+    cy.noFirstNameErrors();
+    cy.noEmailErrors();
+    cy.noPasswordErrors();
+  });
+
+  it("tests the correct error message is returned when a string longer than 55 characters is entered into the last_name field", () => {
+    // Mount the register component
+    cy.mount(Register);
+
+    // Check there are no error messages
+    cy.noErrors();
+
+    // Get the first name field and enter valid data into it
+    cy.get('[data-cy="firstNameInput"]').type("Ben");
+
+    // Get the last name field and enter a string 56 characters long into it
+    cy.get('[data-cy="lastNameInput"]')
+      .invoke("val", stringGen(56))
+      .type("!")
+      .invoke("val")
+      .should("have.length", 56);
+
+    // Get the email field and enter valid data into it
+    cy.get('[data-cy="emailInput"]').type("ben@gmail.com");
+
+    // Get the password field and enter valid data into it
+    cy.get('[data-cy="passwordInput"]').type("password");
+
+    // Check the last name error message was returned
+    cy.shouldShowFieldError([
+      "lastName",
+      "Last name can not be longer than 55 characters",
+    ]);
+
+    // Check the other fields have no error messages
+    cy.noFirstNameErrors();
+    cy.noEmailErrors();
+    cy.noPasswordErrors();
   });
 });
