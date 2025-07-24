feb1159e8e58a1700b07bf6bf87e6f6413abec0f
Ben-1819 <ben.brown20332@gmail.com>
2025-07-24 21:31:51 +0100

    feat: Added the login component to the vue router.

--- FriendPoints-Frontend/src/main.js
+++ FriendPoints-Frontend/src/main.js
@@ -6,11 +6,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import axios from "axios";
 import App from "./App.vue";
 import Register from "./components/Register.vue";
+import Login from "./components/Login.vue";
 
 window.axios = axios;
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/register", component: Register }],
+  routes: [
+    { path: "/register", component: Register },
+    { path: "/login", component: Login },
+  ],
 });
 createApp(App).use(router).use(createPinia()).mount("#app");
