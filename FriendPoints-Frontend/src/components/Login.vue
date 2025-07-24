7ba069064a73f7266b0f03dfd8c5313b75c75e2d
Ben-1819 <ben.brown20332@gmail.com>
2025-07-24 21:22:54 +0100

    feat: Created the login component.

---
+++ FriendPoints-Frontend/src/components/Login.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <h1 id="title">Login To Your Account</h1>
+    <form @submit.prevent="attemptLogin">
+      <div class="flexColumn">
+        <div class="group">
+          <input
+            type="email"
+            v-model="email"
+            data-cy="emailInput"
+            placeholder="Enter your email here"
+            @input="clearFieldError('email')"
+          />
+          <span
+            v-if="validationErrors.email"
+            data-cy="emailError"
+            class="validationError"
+            >{{ validationErrors.email }}</span
+          >
+        </div>
+        <div class="group">
+          <input
+            type="password"
+            v-model="password"
+            data-cy="passwordInput"
+            placeholder="Enter your password here"
+            @input="clearFieldError('password')"
+          />
+
+          <span
+            v-if="validationErrors.password"
+            data-cy="passwordError"
+            class="validationError"
+          >
+            {{ validationErrors.password }}
+          </span>
+        </div>
+        <div class="bottom-buttons">
+          <button id="register-button" data-cy="register-button">Login</button>
+          <router-link to="/register">Need to register for an account? Click here</router-link>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { useAuthStore } from "@/stores/auth";
+import { mapStores } from "pinia";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      validationErrors: {},
+    };
+  },
+  computed: {
+    ...mapStores(useAuthStore),
+  },
+  methods: {
+    // register method, allows the user to make a post request and register their account
+    async attemptLogin() {
+      try {
+        // Make a post request to the login route
+        const response = await axios
+          .post("http://127.0.0.1:8000/api/login", {
+            // Send the email variable in the email field
+            email: this.email,
+            // Send the password variable in the password field
+            password: this.password,
+          })
+          .then((response) => {
+            // Create a variable called token and set it to the token from the response data
+            const token = response.data.token;
+            // Create a variable called user and set it to the user from the response data
+            const user = response.data.user;
+            // Set the user in the auth store to the user from the response data
+            this.authStore.setUser(response.data.user);
+            // Set the token in the auth store to the variable token
+            this.authStore.setToken(token);
+            // Store the token in the browsers local storage
+            localStorage.setItem("token", token);
+          });
+      } catch (error) {
+        // Log the error to the console
+        console.log(`An error has occured ${error}`);
+
+        // Check if there were validation errors
+        if (error.response && error.response.status === 422) {
+          // Set the validation errors to the validation errors returned in the response
+          this.validationErrors = error.response.data.errors;
+        }
+      }
+    },
+    // clearFieldError method - clears the error messages whenever you start typing
+    clearFieldError(field) {
+      if (this.validationErrors[field]) {
+        delete this.validationErrors[field];
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#title {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.bottom-buttons {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.bottom-buttons button {
+  border-radius: 15%;
+  font-size: 1.2rem;
+}
+
+.flexcolumn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.flexcolumn input {
+  margin-top: 5px;
+  margin-bottom: 5px;
+  border-radius: 15%;
+  border: 1px solid white;
+  background-color: black;
+  color: white;
+  height: 30px;
+}
+
+.focused {
+  border-color: #10b981;
+  background-color: #f0fff4;
+}
+
+#register-button {
+  background-color: #10b981;
+  border: none;
+}
+
+.validationError {
+  color: red;
+  font-style: oblique;
+}
+</style>
