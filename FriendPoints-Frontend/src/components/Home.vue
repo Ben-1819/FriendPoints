61765b17a22db8051f0197c5fc863aa2a3451b7f
Ben-1819 <ben.brown20332@gmail.com>
2025-07-28 08:48:24 +0100

    feat: Created the Home component.
    Created the home component, this component will what the user is shown
    directly after they register and allows the user to navigate towards
    every part of the application.

---
+++ FriendPoints-Frontend/src/components/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h1 class="title">Friend Points</h1>
+    <h2 class="title">Welcome {{ authStore.user.first_name }}</h2>
+    <div class="buttonGroup">
+      <button data-cy="allUsersBtn">All Users</button>
+      <button data-cy="friendsBtn">Your Friends</button>
+    </div>
+    <div class="buttonGroup">
+      <button data-cy="rankingsBtn">Current Rankings</button>
+      <button data-cy="logoutBtn" @click="logout">Logout</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+
+let user = ref({});
+
+// Create a constant called router and set it to the useRouter method
+const router = useRouter();
+// Create a constant authStore and set it to the useAuthStore method
+const authStore = useAuthStore();
+
+// Whenever the component is mounted
+onMounted(() => {
+  // Get the token from local storage.
+  const token = localStorage.getItem("token");
+  // If the token has a value
+  if (token) {
+    // Set the token in the auth store to the token retrieved from the browsers local storage.
+    authStore.setToken(token);
+    // Call the fetchUser action in the authStore
+    authStore.fetchUser();
+  }
+  //user = authStore.fetchUser();
+});
+
+const allUsers = () => {};
+
+const yourFriends = () => {};
+
+const currentRankings = () => {};
+
+const logout = () => {
+  authStore.logout();
+  router.push("/login");
+};
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.buttonGroup {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.buttonGroup button {
+  border-radius: 15%;
+  font-size: 1.2rem;
+  background-color: #10b981;
+  border: none;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+</style>
