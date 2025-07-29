d708ef1f1270fb2068e4a32d344952349773a797
Ben-1819 <ben.brown20332@gmail.com>
2025-07-29 17:00:52 +0100

    feat: Added the index method of the UserController to routes
    Created a route that will access the the index method of the
    UserController, this will allow users to retrieve a list of the users on
    the frontend of the application.

--- FriendPointsBackend/routes/api.php
+++ FriendPointsBackend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function(){
     Route::post("/register", "register");
@@ -13,6 +14,10 @@ Route::controller(AuthController::class)->group(function(){
 Route::controller(AuthController::class)->middleware("jwt")->group(function(){
     Route::get("/user", "getUser");
     Route::post("/logout", "logout");
+});
+
+Route::controller(UserController::class)->middleware("jwt")->group(function(){
+    Route::get("/userIndex", "index");
 });
 
 Route::controller(FriendController::class)->middleware("jwt")->group(function(){
