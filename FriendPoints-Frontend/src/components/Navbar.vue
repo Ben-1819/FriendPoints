e7073b8660462610e5c6739302ababc785b9cd24
Ben-1819 <ben.brown20332@gmail.com>
2025-10-12 13:28:58 +0100

    Feat: Created the Navbar component and wrote the script tag

---
+++ FriendPoints-Frontend/src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+
+const authStore = useAuthStore();
+
+const router = useRouter();
+
+onMounted(() => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    authStore.setToken(token);
+    authStore.fetchUser();
+  } else if (token === null) {
+    logout();
+  }
+});
+
+const goToHome = () => {
+  router.push({
+    name: "Home",
+  });
+};
+
+const goToYourFriends = () => {
+  router.push({
+    name: "AllFriends",
+  });
+};
+
+const goToCurrentRankings = () => {
+  router.push({
+    name: "CurrentRankings",
+  });
+};
+
+const goToAllHistories = () => {
+  router.push({
+    name: "AllHistories",
+  });
+};
+
+const logout = () => {
+  authStore.logout();
+  router.push({
+    name: "Login",
+  });
+};
+</script>
+
+<style lang="scss" scoped></style>
