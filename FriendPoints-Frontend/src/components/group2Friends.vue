6108ab01d50883baf87d4985845578421a6f0a99
Ben-1819 <ben.brown20332@gmail.com>
2025-10-13 09:19:33 +0100

    Feat: Refactored group2Friends

--- FriendPoints-Frontend/src/components/group2Friends.vue
+++ FriendPoints-Frontend/src/components/group2Friends.vue
@@ -4,14 +4,16 @@
   </div>
   <div>
     <div class="topscreen">
-      <button class="abutton" @click="home">Home</button>
       <h1 class="title">Group 2 friends</h1>
     </div>
     <div class="group2List">
       <ul>
         <li v-for="friend in group2Friends" :key="friend.id">
           {{ friend.first_name }} {{ friend.last_name }} - Points:
-          {{ friend.points }} <button class="abutton">Edit friend</button>
+          {{ friend.points }}
+          <button class="abutton" @click="goToEditFriend(friend.id)">
+            Edit friend
+          </button>
         </li>
       </ul>
     </div>
@@ -48,15 +50,9 @@ onMounted(() => {
 
     getGroup2Friends;
   } else if (token === null) {
-    // If the token is null send the user back to the login screen
-    router.push("/login");
+    logout();
   }
 });
-
-// Home method - Takes the user back to the home page
-const home = () => {
-  router.push("/home");
-};
 
 // getGroup2Friends Method - Gets all of the users group 2 friends
 const getGroup2Friends = async () => {
@@ -75,8 +71,22 @@ const getGroup2Friends = async () => {
   }
 };
 
+const logout = () => {
+  authStore.logout();
+  router.push({
+    name: "Login",
+  });
+};
+
 // editFriend method - Takes the user to the edit friend menu
-const editFriend = () => {};
+const editFriend = (id) => {
+  router.push({
+    name: "EditFriendMenu",
+    params: {
+      id: id,
+    },
+  });
+};
 </script>
 
 <style scoped>
